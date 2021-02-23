8c8785ac468f5370c1168d27c6f7294e142cc808
hakkı karakaş <hakkikarks@gmail.com>
2021-02-24 00:18:30 +0300

    Kontrol edilecek

--- Console/Program.cs
+++ Console/Program.cs
@@ -8,11 +8,19 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            foreach (var car in carManager.GetAll())
+            ICarService carManager = new CarManager(new EfCarDal(), new RulesManager());
+            IBrandService brandService = new BrandManager(new EfBrandDal());
+            IColorService colorService = new ColorManager(new EfColorDal());
+
+            Car car = new Car() { BrandId = 1, ColorId = 1, DailyPrice = 200, Description = "Audi", ModelYear = 2009 };
+
+            carManager.Add(car);
+
+            foreach (var item in carManager.GetAll())
             {
-                System.Console.WriteLine(car.BrandId);
+                System.Console.WriteLine(item.Description);
             }
+
         }
     }
 }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+
+    }
+}
